c1559cefe5ad0be83954eb8e3d80fa5689b5679c
kirillbelyaitse <kirillbelyaitsev@yandex.com>
2026-03-11 00:15:29 +0300

    server.cpp, доб MSG_WELCOME и основной цикл обработки сообщений

--- lab2/server.cpp
+++ lab2/server.cpp
@@ -67,8 +67,62 @@ int main() {
     std::cout << "[" << inet_ntoa(clientAddr.sin_addr) << ":" 
               << ntohs(clientAddr.sin_port) << "]: " << msg.payload << std::endl;
 
+    Message welcomeMsg;
+    std::memset(&welcomeMsg, 0, sizeof(welcomeMsg));
+    welcomeMsg.type = MSG_WELCOME;
+    std::string welcomeText = "Welcome " + std::string(inet_ntoa(clientAddr.sin_addr)) + 
+                              ":" + std::to_string(ntohs(clientAddr.sin_port));
+    welcomeMsg.length = 1 + welcomeText.length();
+    std::strncpy(welcomeMsg.payload, welcomeText.c_str(), MAX_PAYLOAD - 1);
+
+    send(clientSocket, &welcomeMsg, sizeof(welcomeMsg), 0);
+
+    while (true) {
+        std::memset(&msg, 0, sizeof(msg));
+        bytesReceived = recv(clientSocket, &msg, sizeof(msg), 0);
+
+        if (bytesReceived == 0) {
+            std::cout << "Client disconnected" << std::endl;
+            break;
+        }
+
+        if (bytesReceived < 0) {
+            std::cerr << "Error: Failed to receive message" << std::endl;
+            break;
+        }
+
+        switch (msg.type) {
+            case MSG_TEXT:
+                std::cout << "[" << inet_ntoa(clientAddr.sin_addr) << ":" 
+                          << ntohs(clientAddr.sin_port) << "]: " << msg.payload << std::endl;
+                break;
+
+            case MSG_PING:
+                std::cout << "Received PING, sending PONG" << std::endl;
+                Message pongMsg;
+                std::memset(&pongMsg, 0, sizeof(pongMsg));
+                pongMsg.type = MSG_PONG;
+                pongMsg.length = 1;
+                send(clientSocket, &pongMsg, sizeof(pongMsg), 0);
+                break;
+
+            case MSG_BYE:
+                std::cout << "Received BYE, closing connection" << std::endl;
+                break;
+
+            default:
+                std::cerr << "Warning: Unknown message type " << (int)msg.type << std::endl;
+                break;
+        }
+
+        if (msg.type == MSG_BYE) {
+            break;
+        }
+    }
+
     close(clientSocket);
     close(serverSocket);
 
+    std::cout << "Server stopped" << std::endl;
     return 0;
 }
