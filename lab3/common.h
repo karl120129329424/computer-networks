cea92baf88586f70790466cf9ad43265342cd3bc
kirillbelyaitse <kirillbelyaitsev@yandex.com>
2026-03-31 23:45:06 +0300

    обн common.h (новые типы сообщений для broadcast)

--- lab3/common.h
+++ lab3/common.h
@@ -5,6 +5,8 @@
 
 #define MAX_PAYLOAD 1024
 #define PORT 8080
+#define MAX_CLIENTS 100
+#define THREAD_POOL_SIZE 10
 
 #pragma pack(push, 1)
 struct Message {
@@ -15,12 +17,15 @@ struct Message {
 #pragma pack(pop)
 
 enum MessageType : uint8_t {
-    MSG_HELLO = 1,    // клиент -> сервер (ник)
-    MSG_WELCOME = 2,  // сервер -> клиент
-    MSG_TEXT = 3,     // текст
+    MSG_HELLO = 1,
+    MSG_WELCOME = 2,
+    MSG_TEXT = 3,
     MSG_PING = 4,
     MSG_PONG = 5,
-    MSG_BYE = 6
+    MSG_BYE = 6,
+    MSG_BROADCAST = 7,
+    MSG_CLIENT_JOIN = 8,
+    MSG_CLIENT_LEAVE = 9
 };
 
 #endif // COMMON_H
